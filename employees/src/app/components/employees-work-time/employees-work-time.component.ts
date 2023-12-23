3fafa1e0711a5d23c97b68be0d0405bba195d8cf
Andrija Eremic <andrija.eremic@gmail.com>
2023-12-23 15:43:04 +0100

    added chart.js

--- employees/src/app/components/employees-work-time/employees-work-time.component.ts
+++ employees/src/app/components/employees-work-time/employees-work-time.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { WorkTimeComponent } from './time-entries/work-time.component';
+import Chart from 'chart.js/auto';
 
 @Component({
   selector: 'app-employees-work-time',
@@ -7,4 +8,25 @@ import { WorkTimeComponent } from './time-entries/work-time.component';
   imports: [WorkTimeComponent],
   templateUrl: './employees-work-time.component.html',
 })
-export class EmployeesWorkTimeComponent {}
+export class EmployeesWorkTimeComponent {
+  public chart: any;
+
+  createChart() {
+    this.chart = new Chart('MyChart', {
+      type: 'doughnut', //this denotes tha type of chart
+      data: {
+        labels: ['VueJs', 'EmberJs', 'ReactJs', 'Angular'],
+        datasets: [
+          {
+            backgroundColor: ['#41B883', '#E46651', '#00D8FF', '#DD1B16'],
+            data: [40, 20, 80, 10],
+          },
+        ],
+      },
+    });
+  }
+
+  ngOnInit(): void {
+    this.createChart();
+  }
+}
